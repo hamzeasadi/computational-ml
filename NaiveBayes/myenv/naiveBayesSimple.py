1324937481b8a1506fde88d1e6bdf4a7223d8c98
Hamze Asadi <hamzeasadi@4c175.local>
2021-11-01 22:39:20 +0000

    create some data for Naive Bayes algorithm

---
+++ NaiveBayes/myenv/naiveBayesSimple.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import sklearn as sk
+
+# tuples are (minutes, number of times)
+# this data show how long you've been late and based on this belated behaviour how many times you
+# missed the train (too_late) and how many times you catch the train (in_time)
+in_time = [(0, 22), (1, 19), (2, 17), (3, 18),
+           (4, 16), (5, 15), (6, 9), (7, 7),
+           (8, 4), (9, 3), (10, 3), (11, 2)]
+too_late = [(6, 6), (7, 9), (8, 12), (9, 17),
+            (10, 18), (11, 15), (12,16), (13, 7),
+            (14, 8), (15, 5)]
+
+X, Y = zip(*in_time)
+X2, Y2 = zip(*too_late)
+
+# visualization of the data distribution for in_time and too too_late
+
+# plt.bar(X, Y, alpha=0.7, width=0.8, label='in_time', color='red')
+# plt.bar(X2, Y2, alpha=0.7, width=0.8, label='too_late', color='blue')
+# plt.xlabel('minutes')
+# plt.ylabel('frequency')
+# plt.legend()
+# plt.show()
+
+in_time_dict = dict(in_time)
+too_late_dict = dict(too_late)
+
+# create a function to give us the chance (Probability) of catching the train
