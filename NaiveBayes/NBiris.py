c4839f90ae14df36d49a9e5bd6c53e366ebe588a
Hamze Asadi <hamzeasadi@4c175.local>
2021-11-03 13:33:27 +0000

    download a adult dataset

--- NaiveBayes/NBiris.py
+++ NaiveBayes/NBiris.py
@@ -9,6 +9,17 @@ from sklearn.naive_bayes import GaussianNB
 
 model = GaussianNB()
 data = datasets.load_iris()
+X = data.data
+Y = data.target
+model.fit(X, Y)
+expected = Y
+predicted = model.predict(X)
+
+print(metrics.classification_report(expected, predicted))
+conf_matric = metrics.confusion_matrix(expected, predicted)
+print(conf_matric)
+plt.imshow(conf_matric)
+plt.show()
 
 
 
