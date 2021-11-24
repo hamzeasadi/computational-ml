a30c2aba7acb9798871d495ef439a022a093f4eb
Hamze Asadi <hamzeasadi@4c175.local>
2021-11-24 18:11:27 +0000

    import required modules for logistic regression

---
+++ pytorch/logisticregression.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+import torchvision
+from torchvision.transforms import transforms
+import os
+from torch import nn as nn
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
