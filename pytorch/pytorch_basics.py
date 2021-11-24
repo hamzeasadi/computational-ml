41dd673eff8d17a393b1a5c70f1fd174b50730fe
Hamze Asadi <hamzeasadi@4c175.local>
2021-11-24 14:24:01 +0000

    autograd example

--- pytorch/pytorch_basics.py
+++ pytorch/pytorch_basics.py
@@ -6,17 +6,42 @@ import torchvision
 from torchvision.transforms import transforms
 
 
-# basic autograd
-x = torch.tensor(np.random.randn(1), dtype=torch.float32)
-w = torch.tensor(np.random.randn(1), requires_grad=True)
-b = torch.randn(1, requires_grad=True)
+# # basic autograd
+# x = torch.tensor(np.random.randn(1), dtype=torch.float32)
+# w = torch.tensor(np.random.randn(1), requires_grad=True)
+# b = torch.randn(1, requires_grad=True)
+#
+# y = w*x + b
+# # calculate grade
+# y.backward()
+#
+# print(f"w grad={w.grad}, b.grad={b.grad}")
 
-y = w*x + b
-# calculate grade
-y.backward()
 
-print(f"w grad={w.grad}, b.grad={b.grad}")
+# autograd example
 
+# x = torch.randn(10, 3)
+#
+# y = torch.randn(10, 2)
+# # print(dir(nn.Linear()))
+# linear = nn.Linear(in_features=3, out_features=2)
+# print(f"w: {linear.weight}, b: {linear.bias}")
+# opt = torch.optim.SGD(linear.parameters(), lr=0.05)
+# criterion = nn.MSELoss()
+# epochs = 100
+# for epoch in range(1, epochs):
+#     y_pre = linear(x)
+#     loss = criterion(y, y_pre)
+#     opt.zero_grad()
+#     loss.backward()
+#     opt.step()
+#     if epoch % 10 ==0:
+#         print(f"epoch = {epoch}, loss={loss.item()}")
+#         print(f'w.grad: {linear.weight.grad}, b.grad: {linear.bias.grad}')
+#
+# with torch.no_grad():
+#     print(f"w: {linear.weight.numpy()}")
+#     print(f"bias: {linear.bias.numpy()}")
 
 
 
