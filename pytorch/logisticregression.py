6b64e856d0de00584467e24604ece8f0aceaac4a
Hamze Asadi <hamzeasadi@4c175.local>
2021-11-24 18:14:51 +0000

    load mnist dataset

--- pytorch/logisticregression.py
+++ pytorch/logisticregression.py
@@ -6,6 +6,9 @@ from torchvision.transforms import transforms
 import os
 from torch import nn as nn
 
+train_dataset = torchvision.datasets.MNIST(root='data/', transform=transforms.ToTensor(), train=True, download=True)
+test_dataset = torchvision.datasets.MNIST(root='data/', train=False, download=True, transform=transforms.ToTensor())
+
 
 
 
