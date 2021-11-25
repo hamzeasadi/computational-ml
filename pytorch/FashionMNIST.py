b433a01c35453520eb1167ca126b17ba6bbb29a5
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 18:46:53 +0000

    set hyper parameters

--- pytorch/FashionMNIST.py
+++ pytorch/FashionMNIST.py
@@ -6,3 +6,9 @@ import torch
 import torchvision
 from torch import nn as nn
 from torchvision.transforms import transforms
+
+# set hyper parameters
+num_classes = 10
+num_epochs = 1
+learning_rate = 1e-3
+batch_size = 100
