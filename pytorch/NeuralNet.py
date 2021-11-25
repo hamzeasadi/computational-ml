2ffb9642448048a1efa62d1abbfe9eaba5922b25
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 07:55:36 +0000

    load train and test dataset

--- pytorch/NeuralNet.py
+++ pytorch/NeuralNet.py
@@ -6,13 +6,24 @@ from torch import nn as nn
 from torchvision.transforms import transforms
 import torchvision
 
-# Hyper-parameters 
+# Hyper-parameters
 input_size = 784
 hidden_size = 500
 num_classes = 10
 num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
+
+# load train and test dataset
+train_dataset = torchvision.datasets.MNIST(root='data/', train=True, download=True)
+test_dataset = torchvision.datasets.MNIST(root='data/', train=False, download=True)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
