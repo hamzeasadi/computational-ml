166a658dd2e0e35f530aaeb19aa2ecd07c24fd5f
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 12:59:16 +0000

    load datasets

--- pytorch/cnnSquenc.py
+++ pytorch/cnnSquenc.py
@@ -10,6 +10,12 @@ from torchvision.transforms import transforms
 # device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+# load datasets
+train_dataset = torchvision.datasets.MNIST(root='data/', train=True, download=True,
+                                        transform=transforms.ToTensor())
+test_dataset = torchvision.datasets.MNIST(root='data/', train=False, download=True,
+                                            transform=transforms.ToTensor())
+                                            
 
 
 
