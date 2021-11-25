978f203d8b9a46e7702e28df66460116ca855064
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 11:57:43 +0000

    create an CNN model

--- pytorch/CNNTorch.py
+++ pytorch/CNNTorch.py
@@ -28,11 +28,33 @@ test_dl = torch.utils.data.DataLoader(dataset=test_dataset, shuffle=True, batch_
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # create an CNN model
-# [batch_size, input_channels, input_height, input_width]
+# [batch_size, input_channels, input_height, input_width]  [Batch, Channels, Height, Width]
 class CNNmodel(nn.Module):
     """This is a simple cnn model"""
     def __init__(self, in_shape, num_classes):
-        self.cnn1 = nn.Conv2(in_features)
+        self.cnn1 = nn.Conv2(in_channels=1, out_channels=16, kernel_size=5, stride-1, padding=2)
+        self.relu = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(num_features=16)
+        self.bn2 = nn.BatchNorm2d(num_features=32)
+        self.maxPool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.fc = nn.Linear(in_features=7*7*32, out_features=num_classes)
+        self.flatten = nn.flatten()
+
+    def forward(self, x):
+        x = self.cnn1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxPool(x)
+        x = self.cnn2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.maxPool(x)
+        # x = x.reshape(x.size()[0], -1)
+        x = self.flatten(x)
+        out = self.fc(x)
+        return out
+        
 
 
 
