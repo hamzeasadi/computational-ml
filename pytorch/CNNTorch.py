e08f95a10709c37b106089c7f19166a7356fbb66
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 09:53:34 +0000

    import required modules

---
+++ pytorch/CNNTorch.py
@@ -0,0 +1,23 @@
+# import required modules
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+from torch import nn as nn
+import torchvision
+from torchvision.transforms import transforms
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
