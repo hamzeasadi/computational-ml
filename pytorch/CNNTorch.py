9237b5cd32efbc40b568ac8a568a108b73b04748
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 09:58:10 +0000

    set Hyper parameters

--- pytorch/CNNTorch.py
+++ pytorch/CNNTorch.py
@@ -7,13 +7,21 @@ from torch import nn as nn
 import torchvision
 from torchvision.transforms import transforms
 
+
+# Hyper parameters
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+learning_rate = 0.001
+
 # load MNIST dataset
 train_dataset = torchvision.datasets.MNIST(root='data/', train=True, download=True,
                                             transform=transforms.ToTensor())
 test_dataset = torchvision.datasets.MNIST(root='data/', train=False, download=True,
                                             transform=transforms.ToTensor())
 
-                                                                                        
+# wraps datasets in a dataloader
+train_dl = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=...)                                                                                 #
 
 
 
