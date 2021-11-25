827c02f8339c54bfc5eb1554653cba9989aeba6b
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 09:56:15 +0000

    load MNIST dataset

--- pytorch/CNNTorch.py
+++ pytorch/CNNTorch.py
@@ -7,6 +7,13 @@ from torch import nn as nn
 import torchvision
 from torchvision.transforms import transforms
 
+# load MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='data/', train=True, download=True,
+                                            transform=transforms.ToTensor())
+test_dataset = torchvision.datasets.MNIST(root='data/', train=False, download=True,
+                                            transform=transforms.ToTensor())
+
+                                                                                        
 
 
 
