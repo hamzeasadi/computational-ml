7590a0d94fb8df9509d5b0889215625959181cab
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 18:44:48 +0000

    import required modules

---
+++ pytorch/FashionMNIST.py
@@ -0,0 +1,8 @@
+# import required modules
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+import torchvision
+from torch import nn as nn
+from torchvision.transforms import transforms
