c987ca736353077da22cb6d725aa3d612ef7c109
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 12:54:49 +0000

    import required packages and librarie

---
+++ pytorch/cnnSquenc.py
@@ -0,0 +1,28 @@
+# import required packages and librarie
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+import torchvision
+from torch import nn as nn
+from torchvision.transforms import transforms
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
