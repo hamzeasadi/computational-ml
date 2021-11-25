1c6dd1c194737cefc2c9804834b7779f551aff0f
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-25 07:51:44 +0000

    set Hyper-parameters

--- pytorch/NeuralNet.py
+++ pytorch/NeuralNet.py
@@ -5,3 +5,15 @@ import torch
 from torch import nn as nn
 from torchvision.transforms import transforms
 import torchvision
+
+# Hyper-parameters 
+input_size = 784
+hidden_size = 500
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+
+if __name__ == '__main__':
+    pass
