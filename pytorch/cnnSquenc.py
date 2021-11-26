c1c817bcf99a536098037f5bb93f5dfdba26f086
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-26 15:45:29 +0000

    make a new training

--- pytorch/cnnSquenc.py
+++ pytorch/cnnSquenc.py
@@ -59,42 +59,93 @@ val_dl = torch.utils.data.DataLoader(dataset=val_set, batch_size=batch_size,
 test_dl = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size)
 
 
-# # define and create a model
-# class CnnModel(nn.Module):
-#     """a simple cnn for minist dataset"""
-#     def __init__(self, num_classes):
-#         super(CnnModel, self).__init__()
-#         self.layer1 = nn.Sequential(
-#         nn.Conv2d(in_channels=1, out_channels=16, stride=1, padding=2, kernel_size=5),
-#         nn.BatchNorm2d(num_features=16),
-#         nn.ReLU(),
-#         nn.MaxPool2d(kernel_size=(2, 2), stride=2)
-#         )
-#         self.layer2 = nn.Sequential(
-#         nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
-#         nn.BatchNorm2d(num_features=32),
-#         nn.ReLU(),
-#         nn.MaxPool2d(kernel_size=(2, 2), stride=2)
-#         )
-#         self.flatten = nn.Flatten()
-#         self.fc = nn.Linear(in_features=7*7*32, out_features=num_classes)
-#
-#     def forward(self, x):
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.flatten(x)
-#         out = self.fc(x)
-#         return out
-#
-#
-#
-# model = CnnModel(num_classes=num_classes)
-# model_path = os.path.join(os.getcwd(), 'data', 'cnnSquenc.ckpt')
-# # define loss and optimizer associated with our model
-# criterion = nn.CrossEntropyLoss()
-# opt = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
-#
-#
+# define and create a model
+class CnnModel(nn.Module):
+    """a simple cnn for minist dataset"""
+    def __init__(self, num_classes):
+        super(CnnModel, self).__init__()
+        self.layer1 = nn.Sequential(
+        nn.Conv2d(in_channels=1, out_channels=16, stride=1, padding=2, kernel_size=5),
+        nn.BatchNorm2d(num_features=16),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        )
+        self.layer2 = nn.Sequential(
+        nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
+        nn.BatchNorm2d(num_features=32),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        )
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(in_features=7*7*32, out_features=num_classes)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.flatten(x)
+        out = self.fc(x)
+        return out
+
+
+
+model = CnnModel(num_classes=num_classes)
+model_path = os.path.join(os.getcwd(), 'data', 'cnnSquenc.ckpt')
+# define loss and optimizer associated with our model
+criterion = nn.CrossEntropyLoss()
+opt = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
+
+def save_ckp(state, is_best, checkpoint_path, best_model_path):
+    f_path = checkpoint_path
+    torch.save(state, f_path)
+    if is_best:
+        best_fpath = best_model_path
+        shutil.copyfile(src=f_path, dst=best_fpath)
+
+def train(epochs, valid_loss_min_in, tdata, vdata, , model, optimizer, criteria, checkpoint_path, best_model_path):
+    valid_loss_min = valid_loss_min_in
+    T_loss = []
+    V_loss = []
+    for epoch in range(epochs):
+        train_loss = 0.0
+        valid_loss = 0.0
+        model.train()
+        for btch_idx, (images, labels) in enumerate(tdata):
+            img = images.to(device)
+            labels = labels.to(device)
+            y_pre = model(img)
+            optimizer.zero_grad()
+            loss = criterion(y_pre, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += (loss.item() - train_loss)/len(labels)
+
+        model.eval()
+        with torch.no_grad():
+            for btch_idx, (images, labels) in enumerate(vdata):
+                img = images.to(device)
+                labels = labels.to(device)
+                y_pre = model(img)
+                loss = criterion(y_pre, labels)
+                valid_loss += ((loss.item() - valid_loss)/len(labels))
+        T_loss.append(train_loss/len(tdata))
+        V_loss.append(valid_loss/len(vdata))
+        print(f"epoch = {epoch+1} train loss = {train_loss}   val loss = {valid_loss}")
+
+        checkpoint = {
+        'epoch': epoch+1,
+        'valid_loss_min': valid_loss,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+        }
+    plt.plot(np.arange(len(T_loss)), T_loss, label='train loss')
+    plt.plot(np.arange(len(T_loss)), V_loss, label='valid loss')
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.show()
+
+
+train()
+
 # def train(data, model, epochs):
 #
 #     for epoch in range(epochs):
