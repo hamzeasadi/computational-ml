257786b240f8accf9ab2008c67c07913629fca1e
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-26 17:05:37 +0000

    set hyper parameters for fashinmnist

--- pytorch/FashionMNIST.py
+++ pytorch/FashionMNIST.py
@@ -6,3 +6,12 @@ import torch
 import torchvision
 from torch import nn as nn
 from torchvision.transforms import transforms
+
+
+# set hyper parameters
+num_classes = 10
+num_epochs = 1
+learning_rate = 1e-3
+batch_size = 100
+input_shape = (1, 28, 28)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
