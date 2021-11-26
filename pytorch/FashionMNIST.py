f8a3a011029b14978928277b0c08ff79672cc82e
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-26 17:27:42 +0000

    visualize the data

--- pytorch/FashionMNIST.py
+++ pytorch/FashionMNIST.py
@@ -33,8 +33,30 @@ train_val_dataset = torchvision.datasets.FashionMNIST(root='data', train=True, d
                                                         transform=transform)
 test_dataset = torchvision.datasets.FashionMNIST(root='data', train=False, download=True,
                                                 transform=transform)
-                                                                                                        
 
+# visualize the data
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(test_dataset), size=(1,)).item()
+    img, label = test_dataset[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
 
 
 
