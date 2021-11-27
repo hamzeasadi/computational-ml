1ea232ac8ac5793d67bfcf6fbcebe07ab525d039
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-27 11:47:32 +0000

    import required modules

---
+++ pytorch/CustomeFashionMNIST.py
@@ -0,0 +1,29 @@
+# import required modules
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import torch
+from torch import nn as nn
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import transforms
+import shutil
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
