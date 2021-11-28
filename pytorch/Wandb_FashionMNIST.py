e9c4df282e946d237ec484f1718d2ff6fe5c9ffb
hamzeasadi <hamzeasadi1@gmail.com>
2021-11-28 08:57:51 +0000

    define paths to save and load data and results

--- pytorch/Wandb_FashionMNIST.py
+++ pytorch/Wandb_FashionMNIST.py
@@ -9,3 +9,27 @@ import wandb
 import torch
 from torchvision import datasets, transforms
 from torch import nn as nn
+
+# define paths to save and load data and results
+data_path = os.path.join(os.getcwd(), 'data')
+checkpoint_name = f"Wand-fashionMNIST-checkpoint.pt"
+best_model_name = f"wand-fashionMNIST-best-model.pt"
+best_model_path = os.path.join(data_path, 'best_model', best_model_name)
+checkpoint_path = os.path.join(data_path, 'checkpoint', checkpoint_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    pass
