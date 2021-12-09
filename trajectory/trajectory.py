e97c9926ec7b5732a4cf70148de39309d64ef71e
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-09 08:04:33 +0000

    create a testLSTM file

--- trajectory/trajectory.py
+++ trajectory/trajectory.py
@@ -11,7 +11,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import os
 import random
-ImportError shutil
+import shutil
+import math
+from torch.autograd import Variable
 
 
 # set random seeds to for consistant algorithm performance check
@@ -32,8 +34,12 @@ batch_size = 100
 epochs = 100
 learning_rate = 1e-2
 sample_size = 4
-num_outputs = 3
+# num_outputs = 3
+num_classes = 1
 min_val_error = np.inf
+num_layers = 2
+input_size = 5
+hidden_size =2
 
 # define and implement save and load checkpoints
 def save_ckp(state, ckp_path, is_best_model, bst_model_path):
@@ -136,11 +142,102 @@ class LstmModel(nn.Module):
         pass
 
 
+def EntropyCal(*x):
+    total = sum(x)
+    e = 0.0
+    for a in x:
+        e += -(a/total)*math.log(a/total, 2)
+    print(e)
+
+
+class LSTM1(nn.Module):
+    """simple lstm"""
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super(LSTM1, self).__init__()
+        self.num_classes = num_classes
+        self.hidden_size = hidden_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.seq_length = seq_length
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.fc_1 = nn.Linear(in_features=hidden_size, out_features=128)
+        self.fc = nn.Linear(in_features=128, out_features=num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        h_0 = Variable(torch.randn(self.num_layers, x.size(0), self.hidden_size))
+        c_0 = Variable(torch.randn(self.num_layers, x.size(0), self.hidden_size))
+        output, (hn, cn) = self.lstm(x, (h_0, c_0))
+        hn = hn.view(-1, self.hidden_size)
+        out = self.relu(hn)
+        out = self.fc_1(out)
+        out = self.relu(out)
+        out = self.fc(out)
+        return out
+
+model = LSTM1(num_classes=1, input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, seq_length=1)
+# define model criterion and optimizer
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
+
+def train(model, data, y_train, test_data, y_test, opt, criterion, epochs):
+    for epoch in range(epochs):
+        train_loss = 0.0
+        eval_loss = 0.0
+        model.train()
+        pre = model(data)
+        print(pre.shape, y_train.shape)
+        loss = criterion(pre, y_train)
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        train_loss = loss.item()
+        model.eval()
+        with torch.no_grad():
+            pre_ = model(test_data)
+            loss_ = criterion(pre_, y_test)
+            eval_loss = loss_.item()
+
+        print(f"train-loss = {train_loss}, eval_loss = {eval_loss}")
+
 
 
 def main():
-    DataPipleline = DataWrangling(data_path=data_path, sample_size=sample_size, batch_size=batch_size)
-    trainDataLoader, testDataLoader = DataPipleline.preProcess()
+    # DataPipleline = DataWrangling(data_path=data_path, sample_size=sample_size, batch_size=batch_size)
+    # trainDataLoader, testDataLoader = DataPipleline.preProcess()
+    df = pd.read_csv(os.path.join(data_path, 'SBUX.csv'), index_col = 'Date', parse_dates=True)
+    plt.style.use('ggplot')
+    # df['Volume'].plot(label='CLOSE', title='Star Bucks Stock Volume')
+    # plt.show()
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, 5:6]
+    # print(f"x-shape = {np.shape(X)}, y-shape = {np.shape(y)}")
+    mm = MinMaxScaler()
+    ss = StandardScaler()
+    X_ss = ss.fit_transform(X)
+    y_mm = mm.fit_transform(y)
+
+    X_train = X_ss[:200, :]
+    X_test = X_ss[200:, :]
+    y_train = y_mm[:200, :]
+    y_test = y_mm[200:, :]
+
+    X_train_tensors = Variable(torch.Tensor(X_train))
+    X_test_tensors = Variable(torch.Tensor(X_test))
+    y_train_tensors = Variable(torch.Tensor(y_train))
+    y_test_tensors = Variable(torch.Tensor(y_test))
+    # print(X_train_tensors.shape)
+
+    X_train_tensors_final = torch.reshape(X_train_tensors, shape=(X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
+    X_test_tensors_final = torch.reshape(X_test_tensors, shape=(X_test_tensors.shape[0], 1, X_test_tensors.shape[1]))
+    train(model=model, data=X_train_tensors_final, y_train=y_train_tensors, test_data=X_test_tensors_final,
+        y_test=y_test_tensors, opt=optimizer, criterion=criterion, epochs=1)
+
+
+
+
+
+
 
 
 
