2dc3e368517a7d6b8e5af0b5f0e33e44bca214b2
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-26 12:57:20 +0000

    arrays challenge numpy

--- numpy_utils.py
+++ numpy_utils.py
@@ -4,6 +4,14 @@ import os
 import itertools as it
 
 
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    reversed_array = numpy.zeros(shape=len(arr), dtype='float')
+    for i in range(len(arr)):
+        reversed_array[i] = float(arr[-(i+1)])
+
+    return reversed_array
 
 
 
