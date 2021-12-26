e7c28966bb762e1443cadcd6be3bf9d373507da7
hamzeasadi <hamzeasadi1@gmail.com>
2021-12-26 12:57:00 +0000

    arrays challenge numpy

---
+++ numpy_utils.py
@@ -0,0 +1,19 @@
+import random
+import numpy as np
+import os
+import itertools as it
+
+
+
+
+
+def main():
+    np_arr = np.empty(shape=(3, 3))
+    print(np_arr)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
