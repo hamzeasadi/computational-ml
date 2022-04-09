adf84b597006da48fd9e386968ad6f05ab98db65
hamzeasadi <hamzeasadi1@gmail.com>
2022-04-09 12:36:57 +0100

    batch multiplication

--- pytorch/tensors.py
+++ pytorch/tensors.py
@@ -1,5 +1,5 @@
+from statistics import mean
 import torch
-
 import numpy as np
 
 
@@ -21,9 +21,13 @@ dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def main():
-
-    print(dev)
-
+    my_tensor = torch.tensor([[1,2,3], [6,4,1]], dtype=torch.float32, requires_grad=False, device=dev)
+    norm_tensor = my_tensor.normal_(mean=0, std=1)
+    
+    btch_t1 = torch.rand(size=(100, 10, 15))
+    btch_t2 = torch.rand(size=(100, 15, 11))
+    out = torch.bmm(btch_t1, btch_t2)
+    print(out.shape)
 
 
 
