122d0a40e0c8e2ea604afb0699dd5f68528287e1
hamzeasadi <hamzeasadi1@gmail.com>
2022-04-09 13:31:06 +0100

    import required files

---
+++ pytorch/FCNN.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn as nn
+import numpy as np
+from torch import optim as optim
+from torch import functional as F 
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import transforms
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
